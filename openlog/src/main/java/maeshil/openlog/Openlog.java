849e91035d396de1c67acc79b0853235fcda2c1f
Lucent <156414317+ttegun@users.noreply.github.com>
2025-05-06 21:53:28 +0900

    Welcome to Openlog.

---
+++ openlog/src/main/java/maeshil/openlog/Openlog.java
@@ -0,0 +1,17 @@
+package maeshil.openlog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Openlog extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
